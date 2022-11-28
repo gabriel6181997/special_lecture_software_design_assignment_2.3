433364dda4f39bc4a8c915a39924d3381769adc8
gabriel6181997 <gabriel6181997@gmail.com>
2022-11-28 19:38:50 +0900

    🐛 Change sample.csv path

--- tests/test_CSVPrinter.py
+++ tests/test_CSVPrinter.py
@@ -10,20 +10,20 @@ class TestCSVPrinter(unittest.TestCase):
     self.assertEqual(2,len(l))
 
   def test_read1(self):
-    printer = CSVPrinter('sample.csv')
+    printer = CSVPrinter('./tests/sample.csv')
     line = printer.read()
     print(line)
     self.assertEqual(2,len(line))
 
   def test_read2(self):
-    printer = CSVPrinter('sample.csv')
+    printer = CSVPrinter('./tests/sample.csv')
     line = printer.read()
     print(line)
     self.assertEqual("sample.csv",len[0][0])
 
   def test_read3(self):
     try:
-      printer = CSVPrinter('sample.csv')
+      printer = CSVPrinter('./tests/sample.csv')
       printer.read()
       unittest.TestCase.fail("This line should not be invoked")
     except Exception as e:
